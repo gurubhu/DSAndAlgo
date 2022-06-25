68ded242641abbaa6d725fb1be1abcfa489d5111
prasag1 <guru.prasad@bsci.com>
2022-06-25 14:25:29 +0530

    Binary Tree methods added

--- DSPractice/src/main/java/com/ds/practice/tree/BinaryTree.java
+++ DSPractice/src/main/java/com/ds/practice/tree/BinaryTree.java
@@ -1,5 +1,7 @@
 package com.ds.practice.tree;
 
+import java.util.Stack;
+
 public class BinaryTree {
 	
 	Node root;
@@ -36,5 +38,62 @@ public class BinaryTree {
 			printCurrentLevel(level-1, root.right);
 		}
 		
+	}
+	
+	public void inorderRec(Node root) {
+		if(root == null)
+			return;
+		
+		inorderRec(root.left);
+		System.out.print(" " + root.data);
+		inorderRec(root.right);
+	}
+	
+	public void preorderRec(Node root) {
+		if(root == null)
+			return;
+		
+		System.out.print(" " + root.data);
+		preorderRec(root.left);	
+		preorderRec(root.right);
+	}
+	
+	public void postorderRec(Node root) {
+		if(root == null)
+			return;
+		
+		postorderRec(root.left);	
+		postorderRec(root.right);
+		System.out.print(" " + root.data);
+	}
+	
+	public void inorder() {
+		if(root == null)
+			return;
+		Stack<Node> stack = new Stack<Node>();
+		Node curr = root;
+
+		while(curr != null || stack.size() > 0) {
+			while(curr != null) {
+				stack.push(curr);
+				curr = curr.left;
+			}
+			curr = stack.pop();
+			System.out.print(" " + curr.data);
+			curr = curr.right;
+		}
+	}
+	
+	public int diameterRec(Node root) {
+		if(root == null)
+			return 0;
+		
+		int lheight = heightRec(root.left);
+		int rheight =  heightRec(root.right);
+		int ldiameter = diameterRec(root.left);
+		int rdiameter = diameterRec(root.right);
+		
+		return Math.max(lheight + rheight + 1, Math.max(ldiameter, rdiameter));
+	
 	}
 }
--- DSPractice/src/test/java/com/ds/practice/tree/BinaryTreeTest.java
+++ DSPractice/src/test/java/com/ds/practice/tree/BinaryTreeTest.java
@@ -23,5 +23,179 @@ public class BinaryTreeTest {
 		tree.root.right = new Node(3);
 		tree.root.left.left = new Node(4);
 		tree.levelOrderRec();
+		
+		BinaryTree tree1 = new BinaryTree(25);
+		tree1.root.left = new Node(15);
+		tree1.root.right = new Node(50);
+		tree1.root.left.left = new Node(10);
+		tree1.root.left.right = new Node(22);
+		tree1.root.left.left.left = new Node(4);
+		tree1.root.left.left.right = new Node(12);
+		tree1.root.left.right.left = new Node(18);
+		tree1.root.left.right.right = new Node(24);
+		tree1.root.right.left = new Node(35);
+		tree1.root.right.right = new Node(70);
+		tree1.root.right.left.left = new Node(31);
+		tree1.root.right.left.right = new Node(44);
+		tree1.root.right.right.left = new Node(66);
+		tree1.root.right.right.right = new Node(90);
+		
+		System.out.println("");
+		
+		tree1.levelOrderRec();
+	}
+	
+	@Test
+	public void inorderRecTest() {
+		BinaryTree tree = new BinaryTree(1);
+		tree.root.left = new Node(2);
+		tree.root.right = new Node(3);
+		tree.root.left.left = new Node(4);
+		tree.root.left.right = new Node(5);
+		tree.inorderRec(tree.root);
+		
+		BinaryTree tree1 = new BinaryTree(25);
+		tree1.root.left = new Node(15);
+		tree1.root.right = new Node(50);
+		tree1.root.left.left = new Node(10);
+		tree1.root.left.right = new Node(22);
+		tree1.root.left.left.left = new Node(4);
+		tree1.root.left.left.right = new Node(12);
+		tree1.root.left.right.left = new Node(18);
+		tree1.root.left.right.right = new Node(24);
+		tree1.root.right.left = new Node(35);
+		tree1.root.right.right = new Node(70);
+		tree1.root.right.left.left = new Node(31);
+		tree1.root.right.left.right = new Node(44);
+		tree1.root.right.right.left = new Node(66);
+		tree1.root.right.right.right = new Node(90);
+		
+		System.out.println("");
+		
+		tree1.inorderRec(tree1.root);
+	}
+	
+	
+	
+	@Test
+	public void preorderRecTest() {
+		BinaryTree tree = new BinaryTree(1);
+		tree.root.left = new Node(2);
+		tree.root.right = new Node(3);
+		tree.root.left.left = new Node(4);
+		tree.root.left.right = new Node(5);
+		tree.preorderRec(tree.root);
+		
+		BinaryTree tree1 = new BinaryTree(25);
+		tree1.root.left = new Node(15);
+		tree1.root.right = new Node(50);
+		tree1.root.left.left = new Node(10);
+		tree1.root.left.right = new Node(22);
+		tree1.root.left.left.left = new Node(4);
+		tree1.root.left.left.right = new Node(12);
+		tree1.root.left.right.left = new Node(18);
+		tree1.root.left.right.right = new Node(24);
+		tree1.root.right.left = new Node(35);
+		tree1.root.right.right = new Node(70);
+		tree1.root.right.left.left = new Node(31);
+		tree1.root.right.left.right = new Node(44);
+		tree1.root.right.right.left = new Node(66);
+		tree1.root.right.right.right = new Node(90);
+		
+		System.out.println("");
+		
+		tree1.preorderRec(tree1.root);
+	}
+	
+	@Test
+	public void postorderRecTest() {
+		BinaryTree tree = new BinaryTree(1);
+		tree.root.left = new Node(2);
+		tree.root.right = new Node(3);
+		tree.root.left.left = new Node(4);
+		tree.root.left.right = new Node(5);
+		tree.postorderRec(tree.root);
+		
+		BinaryTree tree1 = new BinaryTree(25);
+		tree1.root.left = new Node(15);
+		tree1.root.right = new Node(50);
+		tree1.root.left.left = new Node(10);
+		tree1.root.left.right = new Node(22);
+		tree1.root.left.left.left = new Node(4);
+		tree1.root.left.left.right = new Node(12);
+		tree1.root.left.right.left = new Node(18);
+		tree1.root.left.right.right = new Node(24);
+		tree1.root.right.left = new Node(35);
+		tree1.root.right.right = new Node(70);
+		tree1.root.right.left.left = new Node(31);
+		tree1.root.right.left.right = new Node(44);
+		tree1.root.right.right.left = new Node(66);
+		tree1.root.right.right.right = new Node(90);
+		
+		System.out.println("");
+		
+		tree1.postorderRec(tree1.root);
+	}
+	
+	@Test
+	public void inorderTest() {
+		BinaryTree tree = new BinaryTree(1);
+		tree.root.left = new Node(2);
+		tree.root.right = new Node(3);
+		tree.root.left.left = new Node(4);
+		tree.root.left.right = new Node(5);
+		tree.inorder();
+		
+		BinaryTree tree1 = new BinaryTree(25);
+		tree1.root.left = new Node(15);
+		tree1.root.right = new Node(50);
+		tree1.root.left.left = new Node(10);
+		tree1.root.left.right = new Node(22);
+		tree1.root.left.left.left = new Node(4);
+		tree1.root.left.left.right = new Node(12);
+		tree1.root.left.right.left = new Node(18);
+		tree1.root.left.right.right = new Node(24);
+		tree1.root.right.left = new Node(35);
+		tree1.root.right.right = new Node(70);
+		tree1.root.right.left.left = new Node(31);
+		tree1.root.right.left.right = new Node(44);
+		tree1.root.right.right.left = new Node(66);
+		tree1.root.right.right.right = new Node(90);
+		
+		System.out.println("");
+		
+		tree1.inorder();
+	}
+	
+	@Test
+	public void diameterTest() {
+		BinaryTree tree = new BinaryTree(1);
+		tree.root.left = new Node(2);
+		tree.root.right = new Node(3);
+		tree.root.left.left = new Node(4);
+		tree.root.left.right = new Node(5);
+		tree.root.left.right.left = new Node(6);
+		tree.root.left.right.right = new Node(7);
+		tree.root.right.right = new Node(8);
+		tree.root.right.right.right = new Node(9);
+		tree.root.right.right.right.left = new Node(10);
+		tree.root.right.right.right.right = new Node(11);
+		tree.root.right.right.right.left.right = new Node(12);
+		assertEquals(9, tree.diameterRec(tree.root));
+		
+		BinaryTree tree1 = new BinaryTree(1);
+		tree1.root.left = new Node(2);
+		tree1.root.right = new Node(3);
+		tree1.root.left.left = new Node(4);
+		tree1.root.left.right = new Node(5);
+		tree1.root.left.left.left = new Node(6);
+		tree1.root.left.left.right = new Node(7);
+		tree1.root.left.left.right.left = new Node(8);
+		tree1.root.left.left.right.left.right = new Node(9);
+		tree1.root.left.right.right = new Node(10);
+		tree1.root.left.right.right.left = new Node(11);
+		tree1.root.left.right.right.right = new Node(12);
+		tree1.root.left.right.right.right.left = new Node(13);
+		assertEquals(9, tree1.diameterRec(tree1.root));
 	}
 }
